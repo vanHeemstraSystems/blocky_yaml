f1f765a3d3ab047a1698eb2c2b28dece4060bac0
Willem van Heemstra <wvanheemstra@icloud.com>
2024-10-27 22:02:05 +0000

    Update

--- hatch-blocky/src/hatch_blocky/__init__.py
+++ hatch-blocky/src/hatch_blocky/__init__.py
@@ -1,3 +1,48 @@
-# SPDX-FileCopyrightText: 2024-present Willem van Heemstra <wvanheemstra@icloud.com>
-#
-# SPDX-License-Identifier: MIT
+from app.extensions import db
+from app.utils.utils_db import init_db
+# from app.models.node import Node
+from config import Config
+from flask import Flask
+from flask_migrate import Migrate
+
+def create_app(config_class=Config):
+    app = Flask(__name__, static_url_path='/static')
+    app.config.from_object(config_class)
+
+    ##
+    # Initialize Flask extensions
+    ##
+
+    # Set db
+    db.init_app(app)
+
+    ##
+    # Migrate database
+    ##
+
+    migrate = Migrate(app,db)
+
+    ##
+    # Register blueprints
+    ##
+
+    from app.routes.main_routes import main_bp
+    app.register_blueprint(main_bp, url_prefix='/')
+
+    # from app.routes.node_routes import node_bp
+    # app.register_blueprint(node_bp, url_prefix='/nodes')
+
+    # from app.routes.yaml_routes import yaml_bp
+    # app.register_blueprint(yaml_bp, url_prefix='/yaml')    
+
+    @app.route('/test/')
+    def test_page():
+        return '<h1>Testing the Flask Application Factory Pattern</h1>'
+
+    # nodezator = Nodezator(app, db, Node)
+
+    with app.app_context():
+        init_db()
+        # nodezator  
+
+    return app
--- hatch-blocky/src/hatch_blocky/app.py
+++ hatch-blocky/src/hatch_blocky/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+# @app.route('/')
+# def hello():
+#     return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
